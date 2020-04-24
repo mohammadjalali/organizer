bec645e8f82b7579f878595c19c56de68c5e2ed9
Mohammad Hosein Jalali <mjalali1378@gmail.com>
2020-04-24 17:59:05 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Orgnize your unix/linux desktop with this scrip
+- Be careful this script can run on unix/linux machines but you can change it for any operating system
+- **How to run: **
+	- type this command in your terminal: **python3 organize.py YOUR-USERNAME**
+feel free and enjoy it
---
+++ organize.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import re
+from numpy import unique
+
+username = sys.argv[1]
+
+path = 'ls /home/{}/Desktop'.format(username)
+
+f = os.popen(path).read()
+
+formats = re.findall(r"\..*", f)
+
+formats = unique(formats)
+
+for fmt in formats:
+    _ = fmt[1:]
+    os.system('mkdir /home/mohammad/Desktop/{}'.format(_))
+    a = re.findall(r"..*\.{}".format(_), f)
+    a = unique(a)
+    print(a)
+    for fi in a:
+        os.system('mv /home/mohammad/Desktop/{} /home/mohammad/Desktop/pic'.format(fi))
